50d953a9983801eb25353a5a87dfe506a4b18df9
Nick <nick@planable.io>
2021-07-16 15:31:07 +0300

    👌 Update deps

--- package.js
+++ package.js
@@ -1,23 +1,21 @@
 Package.describe({
-  name: 'verso:intercom',
+  name: "planable:intercom",
   summary: "Intercom.io basic integration (uses 'widget' snippet)",
-  version: '1.5.0',
-  git: 'https://github.com/versolearning/meteor-intercom.git'
+  version: "1.5.1",
+  git: "https://github.com/Planable/meteor-intercom.git",
 });
 
-Npm.depends({ 'intercom-client': '2.8.3' });
+Npm.depends({ "intercom-client": "2.8.3" });
 
-Package.onUse(function(api) {
-  if (api.versionsFrom)
-    api.versionsFrom('1.3');
+Package.onUse(function (api) {
+  api.versionsFrom("2.3");
+  api.use(["underscore", "accounts-base", "ecmascript"]);
+  api.use(["session", "tracker"], "client");
 
-  api.use(['underscore', 'accounts-base', 'ecmascript']);
-  api.use(['session', 'tracker'], 'client');
+  api.addFiles("intercom_server.js", "server");
+  api.addFiles(["intercom_loader.js", "intercom_client.js"], "client");
 
-  api.add_files('intercom_server.js', 'server');
-  api.add_files(['intercom_loader.js', 'intercom_client.js'], 'client');
-
-  api.export('IntercomSettings');
-  api.export('IntercomHash', 'server');
-  api.export('IntercomClient', 'server');
+  api.export("IntercomSettings");
+  api.export("IntercomHash", "server");
+  api.export("IntercomClient", "server");
 });
